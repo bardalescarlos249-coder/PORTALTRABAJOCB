e2ea0d7a49544f23b699df9802eafe1684d7ac4c
bardalescarlos249-coder <bardalescarlos249@gmail.com>
2026-05-03 17:10:11 -0500

    LANZAMIENTO OFICIAL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,3 +1,3 @@
 export const apiBase = process.env.NODE_ENV === 'production' 
-  ? 'https://job-intelligence-api.herokuapp.com' // Cambia esto a la URL de tu API en prod si vas a hostear el backend
+  ? 'https://portaltrabajocb.onrender.com' 
   : 'http://localhost:8000';
